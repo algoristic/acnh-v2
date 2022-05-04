bf894f26bdff55a82967000468e0dc493ccc6157
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-05-04 15:58:28 +0200

    🚧 Display raw animals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Animal } from './model/animal';
+import { DataService } from './service/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'acnh-v2';
+export class AppComponent implements OnInit {
+  public animals: Animal[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getAnimals()
+      .subscribe(animals => {
+        this.animals = animals
+      });
+  }
 }
